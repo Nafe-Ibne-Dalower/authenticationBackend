88e2fe10d14ac1bea1d788242e457c60d9fca69a
Nafe <121440579+Nafe-Ibne-Dalower@users.noreply.github.com>
2023-11-06 22:13:29 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectToMongo = async function main(){
   try{
-    await mongoose.connect('mongodb://127.0.0.1:27017/inotebook');
+    await mongoose.connect('mongodb://127.0.0.1:27017/authenticationBackend');
   }catch{
     console.log(err)
   }
